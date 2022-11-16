ef1628e45c67ff4957aaa66092d30e39a2e2f7d5
Emil Calonius <emil.calonius@tuni.fi>
2022-11-16 14:14:57 +0200

    feat: log out

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,21 @@
 import Welcome from '../components/Welcome.vue';
 import Search from '../components/Search.vue';
 import { RouterLink } from 'vue-router';
+import { isLoggedIn } from '@/utils/jwtUtil';
+
+function logout() {
+  document.cookie = "token=;expires=" + new Date(0).toUTCString();
+  document.location.reload()
+}
 </script>
 
 <template>
   <main>
-    <div class="login-register-btns">
+    <div class="login-register-btns" v-if="!isLoggedIn()">
       <RouterLink to="/register">Sign up</RouterLink>
       <RouterLink class="login" to="/login">Log in</RouterLink>
     </div>
+    <button v-if="isLoggedIn()" class="logout" @click="logout()">Log out</button>
     <Welcome />
     <Search />
     <RouterLink to="/profile">Profile</RouterLink>
@@ -22,7 +29,7 @@ import { RouterLink } from 'vue-router';
   gap: 1rem;
 }
 
-.login {
+.login, .logout {
   background-color: hsla(160, 100%, 37%, 1);
   border-radius: 10px;
   color: var(--vt-c-black);;

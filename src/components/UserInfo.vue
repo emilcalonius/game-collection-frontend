f4e0c6560b5ca311bcadd0dcde1a095ceee7f334
Emil Calonius <emil.calonius@tuni.fi>
2022-11-21 03:51:09 +0200

    feat: add default blank avatar to profile view

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,11 +1,18 @@
 <script lang="ts">
+import defaultAvatar from '../assets/images/blank_avatar.png';
+
 export default {
   props: {
-    user: Object
+    user: Object,
   },
   methods: {
     clickEditButton() {
       this.$emit('click-show-edit', true);
+    }
+  },
+  data() {
+    return {
+      defaultAvatar: defaultAvatar
     }
   }
 }
@@ -14,7 +21,9 @@ export default {
 <template>
   <div class="user-info-container" v-if="user !== undefined">
     <h2>Avatar:</h2>
-    <img :src="user.avatar" alt="profile picture">
+    <div class="avatar-container">
+      <img class="avatar" :src="user.avatar === '' ? defaultAvatar : user.avatar" alt="profile picture">
+    </div>
     <h2>Username:</h2>
     <p>{{user.name}}</p>
     <h2>Bio:</h2>
@@ -33,5 +42,18 @@ export default {
 
 button {
   width: 5rem;
+}
+
+.avatar {
+  object-fit: cover;
+  width: 10rem;
+  height: 10rem;
+}
+
+.avatar-container {
+  width: 10rem;
+  height: 10rem;
+  overflow: hidden;
+  border-radius: 50%;
 }
 </style>

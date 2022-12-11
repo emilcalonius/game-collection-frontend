27d58c8ca20bf5a861af822898624de56cbb6507
Emil Calonius <emil.calonius@tuni.fi>
2022-12-12 00:24:54 +0200

    build: change router history to hash mode

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/game-collection-frontend',
+  // base: '/game-collection-frontend/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
